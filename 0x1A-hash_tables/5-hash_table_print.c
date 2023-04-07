aa6c3e878e53d3e7e15cbf13d7dbcab2414561b9
Sanderson Nyange <sandersonnyange@gmail.com>
2023-04-07 14:42:14 -0700

    a function that prints a hash table.

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,24 +7,24 @@
  * Description: Key/value pairs are printed in the order
  *              they appear in the array of hash table.
  */
-void hash_table_print(const hash_table_t *htbl)
+void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node;
 	unsigned long int i;
 	unsigned char comma_flag = 0;
 
-	if (htbl == NULL)
+	if (ht == NULL)
 		return;
 
 	printf("{");
-	for (i = 0; i < htbl->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
-		if (htbl->array[i] != NULL)
+		if (ht->array[i] != NULL)
 		{
 			if (comma_flag == 1)
 				printf(", ");
 
-			node = htbl->array[i];
+			node = ht->array[i];
 			while (node != NULL)
 			{
 				printf("'%s': '%s'", node->key, node->value);
